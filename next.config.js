e0f8a253ec72b2072c37e4f8febf3e6d0972e43a
Luiz <luiz.felipe.c.lopes@accenture.com>
2022-02-03 08:36:27 -0300

    implementing docker solution

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  swcMinify: false,
+	  webpackDevMiddleware: config => {
+	    config.watchOptions = {
+	      poll: 1000,
+	      aggregateTimeout: 300,
+	    }
+
+	    return config
+	  },
 }
